8185f096aea86459beb1956b9a8a54716bff53a1
Bruno Silva <brunoosilva@outlook.com>
2021-11-01 10:30:11 -0300

    refactor: ren app

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,5 +1,4 @@
-import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import app from './app'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(app(), document.getElementById('root'))
