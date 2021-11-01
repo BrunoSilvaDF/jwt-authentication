a3ea8c1cb02aa99400af0e951af709b1c2c897e9
Bruno Silva <brunoosilva@outlook.com>
2021-11-01 12:04:00 -0300

    feat: add register

--- web/src/pages/register-page.tsx
+++ web/src/pages/register-page.tsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { RouteComponentProps } from 'react-router-dom'
+import { useRegisterMutation } from '../generated/graphql'
 
 interface RegisterPageProps {}
 
-export const RegisterPage: React.FC<RegisterPageProps> = ({}) => {
-  return <div>Register Page</div>
-}
+export const RegisterPage: React.FC<RouteComponentProps & RegisterPageProps> =
+  ({ history }) => {
+    const [register] = useRegisterMutation()
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState('')
+
+    return (
+      <div>
+        <h3>Register</h3>
+        <form
+          onSubmit={async e => {
+            e.preventDefault()
+            const res = await register({
+              variables: {
+                email,
+                password,
+              },
+            })
+            if (!res.data?.register) {
+              setError('something went wrong')
+            }
+            history.push('/')
+          }}
+        >
+          <input
+            value={email}
+            placeholder='email'
+            onChange={e => setEmail(e.target.value)}
+          />
+          <input
+            value={password}
+            placeholder='password'
+            onChange={e => setPassword(e.target.value)}
+          />
+          <button type='submit'>register</button>
+        </form>
+        <span>{error}</span>
+      </div>
+    )
+  }
