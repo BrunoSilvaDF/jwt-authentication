678a46ec5eba8112e29a85690b33ff93b788cd56
Bruno Silva <brunoosilva@outlook.com>
2021-11-01 14:40:42 -0300

    feat: persisting user session

--- web/src/apollo-client.ts
+++ web/src/apollo-client.ts
@@ -2,12 +2,12 @@ import {
   ApolloClient,
   createHttpLink,
   InMemoryCache,
-  ApolloLink,
+  from,
 } from '@apollo/client'
 import { setContext } from '@apollo/client/link/context'
 import { TokenRefreshLink } from 'apollo-link-token-refresh'
 import jwtDecode, { JwtPayload } from 'jwt-decode'
-import { getAccessToken } from './accessToken'
+import { getAccessToken, setAccessToken } from './accessToken'
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000/graphql',
@@ -24,32 +24,39 @@ const authLink = setContext((_, { headers }) => {
   }
 })
 
-// const tokenRefresher = ApolloLink.from([
-//   new TokenRefreshLink({
-//     accessTokenField: 'accessToken',
-//     isTokenValidOrUndefined: () => {
-//       const token = getAccessToken()
-//       if (!token) {
-//         return true
-//       }
-//       try {
-//         const { exp } = jwtDecode<JwtPayload>(token)
-//         if (exp && Date.now() >= exp * 1000) {
-//           return false
-//         }
-//         return true
-//       } catch {
-//         return false
-//       }
-//     },
-//     fetchAccessToken: () => Promise.resolve({}),
-//     handleFetch: (accessToken: string) => {},
-//   })
-// ])
+const tokenRefresherLink = new TokenRefreshLink({
+  accessTokenField: 'accessToken',
+  isTokenValidOrUndefined: () => {
+    const token = getAccessToken()
+    if (!token) {
+      return true
+    }
+    try {
+      const { exp } = jwtDecode<JwtPayload>(token)
+      if (exp && Date.now() >= exp * 1000) {
+        return false
+      }
+      return true
+    } catch {
+      return false
+    }
+  },
+  fetchAccessToken: () => {
+    return fetch('http://localhost:4000/refresh-token', {
+      method: 'post',
+      credentials: 'include',
+    })
+  },
+  handleFetch: (accessToken: string) => setAccessToken(accessToken),
+  handleError: err => {
+    console.warn('Your refresh token is invalid. Try to relogin')
+    console.error(err)
+  },
+})
 
 export default new ApolloClient({
   uri: 'http://localhost:4000/graphql',
   credentials: 'include',
   cache: new InMemoryCache(),
-  link: authLink.concat(httpLink),
+  link: from([tokenRefresherLink, authLink, httpLink]),
 })
